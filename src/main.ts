30fca192151ae03cea989aff0887b139a6c388e3
liaxum <texier.r@gmail.com>
2022-06-02 17:32:27 +0200

    @upd cors

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
 	});
 
 	app.enableCors({
-		'origin': 'https://liaxum.github.io',
+		'origin': ['https://liaxum.github.io', 'htpp://localhost:8080'],
 		'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
 		'allowedHeaders': 'Authorization, Content-Type, Accept',
 		'credentials': true,
