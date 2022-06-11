e2dd277faaf91debfb6affaca606bcabb5c124b5
liaxum <texier.r@gmail.com>
2022-06-11 19:00:46 +0200

    sim erreur CORS

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,13 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { readFileSync } from 'fs';
 
 async function bootstrap() {
-	/* const httpsOptions = {
-		key: readFileSync('/home/liaxum/dev/green-your-look/secrets/selfsigned.key'),
-		cert: readFileSync('/home/liaxum/dev/green-your-look/secrets/selfsigned.crt')
-	}; */
 
-	const app = await NestFactory.create(AppModule, {
-		//httpsOptions
-	});
+	const app = await NestFactory.create(AppModule);
 
 	app.enableCors({
-		'origin': ['https://liaxum.github.io', 'http://localhost:8080'],
+		'origin': ['https://liaxum.github.io'],
 		'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
 		'allowedHeaders': 'Authorization, Content-Type, Accept',
 		'credentials': true,
