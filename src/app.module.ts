c70f6105e311974f89a2fe4337bbd7ab88d6e717
Romain <texier.r@gmail.com>
2022-09-29 18:18:14 +0200

    rassemblement front et back

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,36 +11,39 @@ import { StylesModule } from './styles/styles.module';
 import { LooksModule } from './looks/looks.module';
 import { ProductsModule } from './products/products.module';
 import { ProductsListsModule } from './products-lists/products-lists.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-	imports: [
-		ConfigModule.forRoot({
-			envFilePath: '.env'
-		}),
-		TypeOrmModule.forRoot({
-			type: 'mariadb',
-			host: process.env.DB_HOST,
-			port: Number(process.env.DB_PORT),
-			username: process.env.DB_USER,
-			password: process.env.DB_PASS,
-			database: process.env.DB_NAME,
-			autoLoadEntities: true,
-			synchronize: true,
-			migrationsRun: true,
-			migrationsTableName: 'migration_history',
-			migrations: [
-				join(__dirname, 'migrations/*{.ts,.js}')
-			],
-		}),
-		UsersModule,
-		AuthModule,
-		CategoriesModule,
-		StylesModule,
-		LooksModule,
-		ProductsModule,
-		ProductsListsModule,
-	],
-	controllers: [AppController],
-	providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../', 'public/dist'),
+      serveRoot: '/',
+    }),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+      migrationsRun: true,
+      migrationsTableName: 'migration_history',
+      migrations: [join(__dirname, 'migrations/*{.ts,.js}')],
+    }),
+    UsersModule,
+    AuthModule,
+    CategoriesModule,
+    StylesModule,
+    LooksModule,
+    ProductsModule,
+    ProductsListsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
